b5611b72da4a01b5e0a019fee138a8873e3fe6d9
lvch <920986596@qq.com>
2023-11-12 00:48:33 +0800

    feat

--- wux-demo-starter/src/main/java/com/wx/demo/starter/WxDemoApplication.java
+++ wux-demo-starter/src/main/java/com/wx/demo/starter/WxDemoApplication.java
@@ -1,4 +1,13 @@
 package com.wx.demo.starter;
 
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WxDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WxDemoApplication.class, args);
+    }
 }
